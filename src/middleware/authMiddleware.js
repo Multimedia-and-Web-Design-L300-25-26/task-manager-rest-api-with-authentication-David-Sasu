34639b3e47ec68fa7d9ec9d1635690113b0050eb
David-Sasu <kofisasu1006@gmail.com>
2026-03-05 19:02:10 +0000

    the final work

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,15 +2,29 @@ import jwt from "jsonwebtoken";
 import User from "../models/User.js";
 
 
-// 1. Extract token from Authorization header
-// 2. Verify token
-// 3. Find user
-// 4. Attach user to req.user
-// 5. Call next()
-// 6. If invalid → return 401
 
-const authMiddleware = async (req, res, next) => {
-  //  implement here
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    if (!token) {
+      throw new Error();
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.id);
+
+    if (!user) {
+      throw new Error();
+    }
+
+    req.user = user;
+    next();
+  }
+catch (error) {   
+   res.status(401).json({ message: 'Unauthorized' });
+  }
 };
 
-export default authMiddleware;
+export default auth;
